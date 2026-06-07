541dcd72bb9918d5e16e2bfabc7c74b1ee2705f6
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2026-06-07 21:49:54 +0100

    improvement or experience and proejcts sections

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,10 @@
 export const projects = [
-  { num: '01', name: 'Easy MLOps',             tags: ['Python', 'FastAPI', 'Docker', 'MLflow'],                    href: 'https://github.com/umbertocicciaa/easy-mlops' },
-  { num: '02', name: 'DevOpsFix',              tags: ['TypeScript', 'Node.js', 'React', 'LLM APIs'],              href: 'https://github.com/umbertocicciaa/devopsfix' },
-  { num: '03', name: 'ChatGPT CLI',            tags: ['Go', 'OpenAI', 'CLI'],                                     href: 'https://github.com/umbertocicciaa/chatgpt-cli' },
-  { num: '04', name: 'Homelab Platform',       tags: ['K3s', 'Grafana', 'Traefik', 'Raspberry Pi'],              href: 'https://github.com/umbertocicciaa/homelab-utils' },
-  { num: '05', name: 'AWS MLOps',              tags: ['AWS', 'SageMaker', 'Terraform', 'Streamlit'],             href: 'https://github.com/umbertocicciaa/aws-mlops' },
-  { num: '10', name: 'QuestMasterAI',          tags: ['Python', 'OpenAI', 'GitHub Actions', 'Docker'],           href: 'https://github.com/umbertocicciaa/QuestMasterAI' },
-  { num: '11', name: 'Instagram Twitter Clone', tags: ['Java', 'Angular', 'PostgreSQL', 'JWT'],                  href: 'https://github.com/umbertocicciaa/InstagramTwitterCloneBackend' },
-  { num: '12', name: 'SongFinder',             tags: ['Kotlin', 'Android', 'Genius API', 'SQLite'],              href: 'https://github.com/umbertocicciaa/SongFinder' },
+  { num: '01', name: 'Italian DBMS', tags: ['DBMS', 'Java', 'DBC', 'UT'], href: 'https://github.com/umbertocicciaa/italian-db' },
+  { num: '02', name: 'Easy MLOps', tags: ['Python', 'FastAPI', 'Docker', 'MLflow'], href: 'https://github.com/umbertocicciaa/easy-mlops' },
+  { num: '03', name: 'DevOpsFix', tags: ['TypeScript', 'Node.js', 'React', 'LLM APIs'], href: 'https://github.com/umbertocicciaa/devopsfix' },
+  { num: '04', name: 'ChatGPT CLI', tags: ['Go', 'OpenAI', 'CLI'], href: 'https://github.com/umbertocicciaa/chatgpt-cli' },
+  { num: '05', name: 'Homelab', tags: ['K3s', 'Grafana', 'Traefik', 'Raspberry Pi'], href: 'https://github.com/umbertocicciaa/homelab-utils' },
+  { num: '06', name: 'AWS MLOps', tags: ['AWS', 'SageMaker', 'Terraform', 'Streamlit'], href: 'https://github.com/umbertocicciaa/aws-mlops' },
+  { num: '07', name: 'QuestMasterAI', tags: ['Python', 'OpenAI', 'GitHub Actions', 'Docker'], href: 'https://github.com/umbertocicciaa/QuestMasterAI' },
+  { num: '09', name: 'SongFinder', tags: ['Kotlin', 'Android', 'Genius API', 'SQLite'], href: 'https://github.com/umbertocicciaa/SongFinder' },
 ]
