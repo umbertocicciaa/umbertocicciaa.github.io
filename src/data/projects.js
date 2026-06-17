48e7573062139a46c9766e759d99167e3b1a05b1
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2026-06-17 19:32:51 +0100

    FIx enumeration

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
   { num: '02', name: 'Easy MLOps', tags: ['Python', 'FastAPI', 'Docker', 'MLflow'], href: 'https://github.com/umbertocicciaa/easy-mlops' },
 //  { num: '03', name: 'DevOpsFix', tags: ['TypeScript', 'Node.js', 'React', 'LLM APIs'], href: 'https://github.com/umbertocicciaa/devopsfix' },
 //  { num: '04', name: 'ChatGPT CLI', tags: ['Go', 'OpenAI', 'CLI'], href: 'https://github.com/umbertocicciaa/chatgpt-cli' },
-  { num: '05', name: 'Homelab', tags: ['K3s', 'Grafana', 'Traefik', 'Raspberry Pi'], href: 'https://github.com/umbertocicciaa/homelab-utils' },
-  { num: '06', name: 'AWS MLOps', tags: ['AWS', 'SageMaker', 'Terraform', 'Streamlit'], href: 'https://github.com/umbertocicciaa/aws-mlops' },
+  { num: '03', name: 'Homelab', tags: ['K3s', 'Grafana', 'Traefik', 'Raspberry Pi'], href: 'https://github.com/umbertocicciaa/homelab-utils' },
+  { num: '04', name: 'AWS MLOps', tags: ['AWS', 'SageMaker', 'Terraform', 'Streamlit'], href: 'https://github.com/umbertocicciaa/aws-mlops' },
 //  { num: '07', name: 'SongFinder', tags: ['Kotlin', 'Android', 'Genius API', 'SQLite'], href: 'https://github.com/umbertocicciaa/SongFinder' },
 ]
